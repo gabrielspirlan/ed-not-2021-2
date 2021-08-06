2b02a89fb4a0e8794183026f4a6792714ba2c6d9
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2021-08-06 23:26:39 +0000

    (06/08) Busca Sequencial e funcoes

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,7 @@
+# Importar o valor da constante pi
+# math é o nome da biblioteca onde pi se encontra
+from math import pi
+
 # Função é um trecho de código que tem um nome e pode receber informações externas para fazer seu trabalho. Opcionalmente, uma função pode também produzir um valor de resultado. Uma função é definida apenas uma vez e pode ser utilizada (chamada) várias vezes, evitando repetições de código.
 # Existem funções já providas pela linguagem, como por exemplo, len(), range() e sort() e podemos definir também nossas próprias funções.
 
@@ -14,10 +18,32 @@ def imc(peso, altura):  #Definição (ou declaração) da função
 
 #float(): converte o valor informado em um número com casas decimais
 #(flaoting point)
-p = float(input('Informe o peso da pessoa: '))
-a = float (input('Informe a altura da pessoa: '))
+#p = float(input('Informe o peso da pessoa: '))
+#a = float (input('Informe a altura da pessoa: '))
 
 #Fazendo uma chamada à função imc()
-resultado = imc(p, a)
-print(resultado)
+#resultado = imc(p, a)
+#print(resultado)
 
+def area_forma(base, altura, forma = 'R'):
+    '''
+        Função que calcula a área de uma das seguintes
+        formas geométricas: retângulo, triângulo ou elipse
+        Parâmetro forma:
+        "R" == retângulo (parâmetro opcional com valor padrão)
+        "T" == triângulo
+        "E" == elipse
+    '''
+    area = 0
+    if forma == 'R': # Retângulo
+        area = base * altura
+    elif forma == 'T': # Triângulo
+        area = base * altura / 2
+    elif forma == 'E':
+        area = (base / 2) * (altura / 2) * pi
+    return area
+
+print(f"Retângulo 7.5x11: {area_forma(7.5, 11, 'R')}")
+print(f"Triângulo 8x12: {area_forma(8, 12, 'T')}")
+print(f"Elipse 15x15: {area_forma(15, 15, 'E')}")
+print(f"Quadrado 9.5x9.5: {area_forma(9.5, 9.5)}") # Seleciona o valor padrão na 3° parametro
