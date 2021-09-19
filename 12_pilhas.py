3458d1aa15a59d59908411f884b23c0f31a7e876
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2021-09-19 15:20:43 +0000

    Atualização dos arquivos no repositório

--- 12_pilhas.py
+++ 12_pilhas.py
@@ -1,5 +1,5 @@
-# Políndro: é um texto que, quando lido de trás para frente,
-# mante´m o mesmo conteúdo (desprezando acentos e espaçamento)
+# Palíndromo: é um texto que, quando lido de trás para frente,
+# mantém o mesmo conteúdo (desprezando acentos e espaçamento)
 # texto = "SOCORRAM-ME, SUBI NO ÔNIBUS EM MARROCOS"
 texto = "BATATINHA QUANDO NASCE ESPALHA RAMA PELO CHÃO"
 
@@ -7,15 +7,25 @@ pilha = []
 
 # Percurso do palíndromo, colocando cada letra na lista
 for letra in texto:
-    pilha.append(letra)
+    pilha.append(letra) # append() sempre acrescenta por 
+    
+# pilha.insert(10, 'y')
+# pilha.insert(19, 'g')
+# pilha.insert(6, 'ç')
 
 print(pilha)
 
 inverso = ""
 
+# Remoção de elementos em posições que não são a final
+#del pilha[11]   # Remove a posição 11
+#del pilha[21]   # Remove a posição 21
+#del pilha[8]
+#del pilha[25]
+
 # Retira cada letra da lista, de trás para frente, e coloca no inverso
 while len(pilha) > 0:
-    inverso += pilha.pop()   # pop() retira sempre o ÚLTIMO elemento
+    inverso += pilha.pop()  # pop() retira sempre o ÚLTIMO elemento
 
 print(inverso)
 
@@ -25,5 +35,6 @@ print(inverso)
 # regra LIFO (last in, first out - último a entrar, primeiro a sair), e tem
 # acesso limitado aos seus elementos.
 
-# 2 + {3 - [5 * (2 + 7)]}
+# 2 + { 3 - [5 * (2 + 7)}]
+
 
--- lib/stack.py
+++ lib/stack.py
@@ -1,43 +1,42 @@
 class Stack:
     """
-        Estruturas de dados pilha
+        ESTRUTURAS DE DADOS PILHA
         - Pilha é uma lista linear de acesso restrito, que permite apenas as operações
         de inserção (push) e retirada (pop), ambas ocorrendo no final da estrutura.
         - Como consequência, a pilha funciona pelo princípio LIFO (Last In, First Out -
         último a entrar, primeiro a sair).
         - A principal aplicação das pilhas são tarefas que envolvem a inversão de uma
-        sequiência de dados.
+        sequência de dados.     
     """
 
+    """
+        Construtor da classe
+    """
     def __init__(self):
-        self.__data =  []        # Inicializa uma lista privada vazia
+        self.__data = []        # Inicializa uma lista privada vazia
 
     """
         Método para inserção
         O nome do método de inserção em pilhas é padronizado: push()
     """
     def push(self, val):
-       self.__data.append(val)
+        self.__data.append(val)
 
     """
         Método para retirada
         O nome do método de retirada em pilhas também é padronizado: pop()
-    """ 
+    """
     def pop(self):
-        if self.is_empty: 
-            return None 
-        else: 
-            return self.__data.pop()
-    
+        if self.is_empty(): return None
+        else: return self.__data.pop()
+
     """
         Método para consultar o topo da pilha (último elemento), sem retirá-lo
-        Nome: padronizado: peek()
+        Nome padronizado: peek()
     """
     def peek(self):
-        if self.is_empty: 
-            return None
-        else: 
-            return self.__data[-1]
+        if self.is_empty(): return None
+        else: return self.__data[-1]
 
     """
         Método para verificar se a pilha está vazia ou não
@@ -45,7 +44,7 @@ class Stack:
     """
     def is_empty(self):
         return len(self.__data) == 0
-    
+
     """
         Método que exibe a pilha como uma string (para fins de depuração)
     """
@@ -55,6 +54,6 @@ class Stack:
             if string != "": string += ", "
             string += str(el)
         return "[ " + string + " ]"
-    
-######################################################################################
+
+#########################################################################
 
